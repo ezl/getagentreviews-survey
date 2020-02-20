04c9d122fdb5bfa2c5b7259c86a091bd44a97ccd
TNORTHERN <traynorthern@yahoo.com>
2020-02-20 11:24:05 -0600

    changing state to a function in vuex store and tidying up class binders

--- components/reviews/RatingStars.vue
+++ components/reviews/RatingStars.vue
@@ -1,36 +1,31 @@
 <template>
-   <div class="ratings__stars d-flex">
-          <span
-            class="ratings__star text-center"
-            v-for="(star, index) in ratings"
-            :key="star.rating"
-          >
-            <i
-              @mouseout="hovered = hovered.filter(each => each !== index)"
-              @mouseover="hovered = [...hovered, index]"
-              @click="$store.commit('reviews/setChosen', star.rating)"
-              :class="[
-                hovered.includes(index) ||
-                hovered[0] >= index ||
-                $store.state.reviews.chosen >= star.rating
-                  ? 'fas fa-star'
-                  : 'far fa-star',
-                $store.state.reviews.chosen >= star.rating
-                  ? 'ratings__star-is-chosen'
-                  : ''
-              ]"
-              class="d-block"
-            ></i>
-            <span :class="$store.state.reviews.chosen === star.rating && 'bold text-purple'" class="text-center ratings__star-text">{{ star.text }}</span>
-          </span>
-        </div>
+  <div class="ratings__stars d-flex">
+    <span
+      class="ratings__star text-center"
+      v-for="(star, index) in ratings"
+      :key="star.rating"
+    >
+      <i
+        @mouseout="hovered = hovered.filter(each => each !== index)"
+        @mouseover="hovered = [...hovered, index]"
+        @click="$store.commit('reviews/setChosen', star.rating)"
+        :class="starBinder(index, star.rating)"
+        class="d-block"
+      ></i>
+      <span
+        :class="textColorBinder(star.rating)"
+        class="text-center ratings__star-text"
+        >{{ star.text }}</span
+      >
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-data () {
+  data() {
     return {
-           ratings: [
+      ratings: [
         { rating: 1, text: 'Awful' },
         { rating: 2, text: 'Not great' },
         { rating: 3, text: 'Okey' },
@@ -39,11 +34,24 @@ data () {
       ],
       hovered: []
     }
-}
+  },
+  methods: {
+    starBinder(index, rating) {
+      if (
+        this.hovered.includes(index) ||
+        this.hovered[0] >= index ||
+        this.$store.state.reviews.chosen >= rating
+      ) {
+        return 'fas fa-star ratings__star-is-chosen'
+      } else {
+        return 'far fa-star'
+      }
+    },
+    textColorBinder(rating) {
+      return this.$store.state.reviews.chosen === rating && 'bold text-purple'
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-
-
-</style>
+<style lang="scss"></style>
--- store/reviews.js
+++ store/reviews.js
@@ -1,40 +1,40 @@
-const state = {
-    chosen: 0,
-    rated: false,
-    leftFeedBackLocal: false,
-    feedback: '',
-    leftFeedBackExternal: false,
-    externalFeedBack: '',
-}
+const state = () => ({
+  chosen: 0,
+  rated: false,
+  leftFeedBackLocal: false,
+  feedback: '',
+  leftFeedBackExternal: false,
+  externalFeedBack: ''
+})
 
 const mutations = {
-    setChosen(state, chosen) {
-        state.chosen = chosen
-    },
-    setRated(state, bool) {
-        if (bool) {
-            state.rated = bool
-        }
-        state.rated = !state.rated
-    },
-    setLeftFeedBackLocal(state, bool) {
-        if (bool) {
-            state.leftFeedBackLocal = bool
-        }
-        state.leftFeedBackLocal = !state.leftFeedBackLocal
-    },
-    setLeftFeedBackExternal(state, bool) {
-        if (bool) {
-            state.leftFeedBackExternal = bool
-        }
-        state.leftFeedBackExternal = !state.leftFeedBackExternal
-    },
-    setFeedback(state, feedback) {
-        state.feedback = feedback
+  setChosen(state, chosen) {
+    state.chosen = chosen
+  },
+  setRated(state, bool) {
+    if (bool) {
+      state.rated = bool
     }
+    state.rated = !state.rated
+  },
+  setLeftFeedBackLocal(state, bool) {
+    if (bool) {
+      state.leftFeedBackLocal = bool
+    }
+    state.leftFeedBackLocal = !state.leftFeedBackLocal
+  },
+  setLeftFeedBackExternal(state, bool) {
+    if (bool) {
+      state.leftFeedBackExternal = bool
+    }
+    state.leftFeedBackExternal = !state.leftFeedBackExternal
+  },
+  setFeedback(state, feedback) {
+    state.feedback = feedback
+  }
 }
 
 export default {
-    state,
-    mutations
+  state,
+  mutations
 }
