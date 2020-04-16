a71a3efc379da4af2b5ec0874e1c7520ae59ed0a
TNORTHERN <traynorthern@yahoo.com>
2020-04-16 13:40:01 -0500

    quick minor updates to mobile

--- components/landing/HowItWorks/TwoPeople.vue
+++ components/landing/HowItWorks/TwoPeople.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container>
     <v-row align="start" justify="center">
-      <v-col sm="6" lg="7" cols="10">
+      <v-col sm="6" lg="7" cols="12">
         <img class="how-it-works-image--double" src="https://getagentreviews.com/_nuxt/img/f7e3360.png" alt="person">
         <div class="mt-9" style="margin-left: 44px;">
           <h2>If the client has a great experience</h2>
           <span class="how-it-works--gray">we follow up and ask them to leave you a review online.</span>
         </div>
       </v-col>
-      <v-col sm="6" lg="5" cols="11">
+      <v-col sm="6" lg="5" cols="12">
         <img class="how-it-works-image--double" src="https://getagentreviews.com/_nuxt/img/c29b252.png" alt="person">
         <div class="mt-9" style="margin-left: 85px;">
           <h2>If the client had a bad experience</h2>
