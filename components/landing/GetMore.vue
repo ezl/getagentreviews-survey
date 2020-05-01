99583f7d15e15d72cd6fe405a500563eaabf1ed5
TNORTHERN <traynorthern@yahoo.com>
2020-05-01 16:45:03 -0500

    adding mobile for get more reviews on landing

--- components/landing/GetMore.vue
+++ components/landing/GetMore.vue
@@ -1,52 +1,56 @@
 <template>
-  <div
-    id="get-more"
-    class="home__get-more d-flex justify-space-between w-100"
-  >
-    <div class="home__get-more-cta align-self-center">
-      <h1>Get More 5 Star Reviews Online</h1>
-      <div class="ml-1 mr-1 ml-sm-0 mr-sm-0">
-        <p>We make it simple and automatic for you to keep getting more 5 star reviews on Zillow, Yelp, or Google.</p>
+  <div>
+    <div
+      id="get-more"
+      class="home__get-more d-flex justify-space-between w-100"
+    >
+      <div class="home__get-more-cta align-self-center">
+        <h1>Get More 5 Star Reviews Online</h1>
+        <div class="ml-1 mr-1 ml-sm-0 mr-sm-0">
+          <p>We make it simple and automatic for you to keep getting more 5 star reviews on Zillow, Yelp, or Google.</p>
+        </div>
+        <nuxt-link to="/register">
+          <button class="button button--landing">
+            Get Started Now
+          </button>
+        </nuxt-link>
+        <div class="mt-8">
+          <span class="grey--text ligthen-1">Get Reviews on</span>
+          <v-container>
+            <v-row
+              justify-xl="start"
+              justify-lg="start"
+              justify-md="start"
+              justify="center"
+              align="center"
+              class="get-more__icons"
+            >
+              <img
+                style="height: width: 85px; height: 28px;"
+                src="@/assets/google.png"
+                alt="google"
+              >
+              <img
+                style="height: width: 96px; height: 20px;"
+                src="@/assets/zillow.png"
+                alt="zillow"
+              >
+              <img
+                src="@/assets/yelp.png"
+                alt="yelp"
+              >
+            </v-row>
+          </v-container>
+        </div>
       </div>
-      <nuxt-link to="/register">
-        <button class="button button--landing">
-          Get Started Now
-        </button>
-      </nuxt-link>
-      <div class="mt-8">
-        <span class="grey--text ligthen-1">Get Reviews on</span>
-        <v-container>
-          <v-row
-            justify-xl="start"
-            justify-lg="start"
-            justify-md="start"
-            justify="center"
-            align="center"
-          >
-            <img
-              style="height: width: 85px; height: 28px;"
-              src="@/assets/google.png"
-              alt="google"
-            >
-            <img
-              style="height: width: 96px; height: 20px;"
-              src="@/assets/zillow.png"
-              alt="zillow"
-            >
-            <img
-              src="@/assets/yelp.png"
-              alt="yelp"
-            >
-          </v-row>
-        </v-container>
+      <div class="home__get-more-image">
+        <img
+          src="@/assets/reviewbg.png"
+          alt="title"
+        >
       </div>
     </div>
-    <div class="home__get-more-image">
-      <img
-        src="@/assets/reviewbg.png"
-        alt="title"
-      >
-    </div>
+    <img src="https://s3-prod.adage.com/s3fs-public/styles/width_1024/public/20190530_iStock-1031924616.jpg" class="get__more-mobile-image" alt="">
   </div>
 </template>
 
@@ -118,11 +122,16 @@ export default {
     font-weight: 50;
   }
 }
-.home__get-more-logos {
-  img {
-    // width: 96px;
-    // height: 20px;
-    // margin: 0 9px;
+.get-more__icons {
+  @include large("down") {
+    justify-content: center !important;
+  }
+}
+.get__more-mobile-image {
+  width: 100%;
+  display: none;
+  @include large("down") {
+    display: block;
   }
 }
 </style>
